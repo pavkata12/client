e172d3e5988fc35d91470a6ab37983e3f7358949
pavkata12 <pavkamoinov69@gmail.com>
2025-05-25 21:49:46 +0300

    Add files via upload

--- client/run_as_admin.bat
+++ client/run_as_admin.bat
@@ -1,5 +1,2 @@
 @echo off
-set SCRIPT_PATH=%~dp0src\lock_screen_main.py
-set PYTHON_PATH=%~dp0..\venv\Scripts\python.exe
-
-powershell -Command "Start-Process '%PYTHON_PATH%' -ArgumentList '%SCRIPT_PATH%' -Verb RunAs -WorkingDirectory '%~dp0src'" 
+powershell -Command "Start-Process cmd -ArgumentList '/c cd /d C:\Users\pavka\Desktop\client-main\client\src && python lock_screen_main.py' -Verb RunAs" 
