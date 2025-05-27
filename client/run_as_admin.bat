acc1f1a4149a8e1c1b1be142c30c6f51be2340a0
pavkata12 <pavkamoinov69@gmail.com>
2025-05-27 21:15:21 +0300

    Add files via upload

--- client/run_as_admin.bat
+++ client/run_as_admin.bat
@@ -1,2 +1,2 @@
 @echo off
-powershell -Command "Start-Process pythonw -ArgumentList 'lock_screen_main.py' -Verb RunAs -WorkingDirectory 'C:\Users\pavka\Desktop\client-main\client\src'" 
+powershell -Command "Start-Process python -ArgumentList 'src/lock_screen_main.py' -WindowStyle Hidden -Verb RunAs" 
